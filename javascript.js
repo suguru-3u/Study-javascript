6b9163476b5cca71406dc3d698bdb59cc96b8dcb
Oohira Suguru <suguru.13579515@gmail.com>
2020-07-23 17:00:04 +0900

    add to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,24 @@
 window.alert('こんにちわ、世界！');
 //コメント
+// エスケープシーケンス
+let x = 'こんにちわ \n よろしく!';
+console.log(x);
+
+// 定数は大文字で定義する
+const tax = 0.1;
+let price = 800;
+let sum = tax * price;
+console.log(sum);
+
+// テンプレート文字列(バッククォートを使用する)
+let name = '田中';
+let str = `こんにちわ,${name}さん`;
+console.log(str);
+
+// 配列
+let date = [0,1,10];
+console.log(date[2]);
+
+// オブジェクトリテラル(ハッシュ,連想配列とも言う)
+let obj = {x:1, y:2, g:3};
+console.log(obj.x);
