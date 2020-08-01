27f8908557b3cefc43f36824f1f75f01b8bf6084
Oohira Suguru <suguru.13579515@gmail.com>
2020-08-01 22:57:47 +0900

    Study Javascript

--- javascript.js
+++ javascript.js
@@ -434,96 +434,120 @@
 //     list.appendChild(br);
 //   },false);
 // },false);
-document.addEventListener('DOMContentLoaded',function(){
-  let elem = document.getElementById('elem');
-  elem.addEventListener('mouseover',function(){
-    this.style.backgroundColor = 'Yellow';
-  },false);
-  elem.addEventListener('mouseout',function(){
-    this.style.backgroundColor = 'red';
-  },false);
-});
-function Triangle(){
-  var _base;
-  var _height;
+// document.addEventListener('DOMContentLoaded',function(){
+//   let elem = document.getElementById('elem');
+//   elem.addEventListener('mouseover',function(){
+//     this.style.backgroundColor = 'Yellow';
+//   },false);
+//   elem.addEventListener('mouseout',function(){
+//     this.style.backgroundColor = 'red';
+//   },false);
+// });
+// function Triangle(){
+//   var _base;
+//   var _height;
+//
+//   Object.defineProperty(
+//     this,
+//     'base',
+//     {
+//       get:function(){
+//         return _base;
+//       },
+//       set:function(base){
+//         if(typeof base === 'number' && base > 0){
+//           _base = base;
+//         }
+//       }
+//     }
+//   );
+//   Object.defineProperty(
+//     this,
+//     'height',
+//     {
+//       get:function(){
+//         return _height;
+//       },
+//       set:function(height){
+//         if(typeof height === 'number' && height > 0){
+//           _height = height ;
+//         }
+//       }
+//     }
+//   );
+// };
+//
+// Triangle.prototype.getArea = function(){
+//   return this.base * this.height /2 ;
+// };
+//
+// var t = new Triangle();
+// t.base = 10;
+// t.height = 2;
+// console.log(t.base)
+// console.log(t.getArea(10,2));
+// let current = new Date;
+// let result = document.getElementById('result');
+// result.textContent = current.toLocaleString();
+//
+// function showmessage(a){
+//   if(arguments.length !== 1){
+//     throw new Error('引数の数が間違っています:' + arguments.length);
+//   }
+//   console.log(a)
+// }
+//
+// try{
+//   showmessage('山田','鈴木');
+// }catch(e){
+//   window.alert(e.message);
+// }
+// function sum(){
+//   let result = 0;
+//   for(let i = 0, len = arguments.length; i < len; i++){
+//     let tmp = arguments[i];
+//     if(typeof tmp !== 'number'){
+//       throw new Error('引数が数値ではありません:' + tmp);
+//     }
+//     result += tmp;
+//   }
+//   return result;
+// }
+// アロー関数
+// let getTriangle = (base,height) => {
+//   return base * height / 2 ;
+// };
+//
+// console.log(`三角形の面積は${getTriangle(5,2)}です。`);
+// let getTriangle = (base,height) =>  base * height / 2 ;
+//
+// console.log(`三角形の面積は${getTriangle(5,2)}です。`);
+//
+// let getCircle = radius => radius * radius * Math.PI;
+// console.log(getCircle(3));
+//
+// let show = () => console.log('こんにちわ');
+window.alert('こんにちわ、世界！');
+//コメント
+// エスケープシーケンス
+let x = 'こんにちわ \n よろしく!';
+console.log(x);
 
-  Object.defineProperty(
-    this,
-    'base',
-    {
-      get:function(){
-        return _base;
-      },
-      set:function(base){
-        if(typeof base === 'number' && base > 0){
-          _base = base;
-        }
-      }
-    }
-  );
-  Object.defineProperty(
-    this,
-    'height',
-    {
-      get:function(){
-        return _height;
-      },
-      set:function(height){
-        if(typeof height === 'number' && height > 0){
-          _height = height ;
-        }
-      }
-    }
-  );
-};
+// 定数は大文字で定義する
+const tax = 0.1;
+let price = 800;
+let sum = tax * price;
+console.log(sum);
 
-Triangle.prototype.getArea = function(){
-  return this.base * this.height /2 ;
-};
+// テンプレート文字列(バッククォートを使用する)
+let name = '田中';
+let str = `こんにちわ,${name}さん`;
+console.log(str);
 
-var t = new Triangle();
-t.base = 10;
-t.height = 2;
-console.log(t.base)
-console.log(t.getArea(10,2));
-let current = new Date;
-let result = document.getElementById('result');
-result.textContent = current.toLocaleString();
+// 配列
+let date = [0,1,10];
+console.log(date[2]);
 
-function showmessage(a){
-  if(arguments.length !== 1){
-    throw new Error('引数の数が間違っています:' + arguments.length);
-  }
-  console.log(a)
-}
-
-try{
-  showmessage('山田','鈴木');
-}catch(e){
-  window.alert(e.message);
-}
-function sum(){
-  let result = 0;
-  for(let i = 0, len = arguments.length; i < len; i++){
-    let tmp = arguments[i];
-    if(typeof tmp !== 'number'){
-      throw new Error('引数が数値ではありません:' + tmp);
-    }
-    result += tmp;
-  }
-  return result;
-}
-アロー関数
-let getTriangle = (base,height) => {
-  return base * height / 2 ;
-};
-
-console.log(`三角形の面積は${getTriangle(5,2)}です。`);
-let getTriangle = (base,height) =>  base * height / 2 ;
-
-console.log(`三角形の面積は${getTriangle(5,2)}です。`);
-
-let getCircle = radius => radius * radius * Math.PI;
-console.log(getCircle(3));
-
-let show = () => console.log('こんにちわ');
+// オブジェクトリテラル(ハッシュ,連想配列とも言う)
+let obj = {x:1, y:2, g:3};
+console.log(obj.x);
